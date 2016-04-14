d82e5b6637aa7b57e9327296da71a2ccb31a72d0
David Thomas <david.thomas@enterprisedb.com>
2016-04-14 12:06:54 -0400

    got alot further this morning
    @vibhorkum can you let me know if i'm heading in the correct direction?

--- partition_fk_management.sql
+++ partition_fk_management.sql
@@ -1,49 +1,38 @@
-CREATE OR REPLACE FUNCTION public.create_fk_constraint(parent_table_name text, parent_table_column_names text, child_table_name text, child_table_column_names text, cascade boolean)
+CREATE OR REPLACE FUNCTION public.create_fk_constraint(parent_table_name regclass, parent_table_column_names text, child_table_name regclass, child_table_column_names text, cascade boolean)
  RETURNS boolean
  LANGUAGE plpgsql
 AS $function$
+
 BEGIN
-
 --parent table
-DROP TRIGGER IF EXISTS AT on create_fk_constraint.parent_table_name;
+EXECUTE 'DROP TRIGGER IF EXISTS fk_constraint on ' || parent_table_name;
 
-CREATE TRIGGER AT BEFORE DELETE OR UPDATE ON parent_table_name FOR EACH ROW
+EXECUTE 'CREATE TRIGGER fk_constraint BEFORE DELETE OR UPDATE ON ' || parent_table_name || ' FOR EACH ROW
 EXECUTE PROCEDURE
-check_foreign_key (2, cascade, parent_table_column_names, child_table_name, child_table_column_names);
-
-/*
-2	- means that check must be performed for foreign keys of 2 tables.
-cascade	- defines that corresponding keys must be deleted.
-ID	- name of primary key column in triggered table (A). You may
-	  use as many columns as you need.
-B	- name of (first) table with foreign keys.
-REFB	- name of foreign key column in this table. You may use as many
-	  columns as you need, but number of key columns in referenced
-	  table (A) must be the same.
-C	- name of second table with foreign keys.
-REFC	- name of foreign key column in this table.
-*/
+check_foreign_key (
+  1,  			-- number of tables that foreign keys need to be checked
+  ' || cascade || ', 	-- boolean defines that corresponding keys must be deleted.
+  ' || quote_ident(parent_table_column_names) || ', 	-- name of primary key column in triggered table (A). 
+							-- You may use as many columns as you need.
+  ' || child_table_name || ', 	-- name of (first) table with foreign keys.
+  ' || quote_ident(child_table_column_names) || ')'; 	-- name of foreign key column in this table. 
+							-- You may use as many columns as you need, 
+							-- but number of key columns in referenced table (A) 
+							-- must be the same.
 
 --child table
-/*
-CREATE TRIGGER BT BEFORE INSERT OR UPDATE ON B FOR EACH ROW
+
+EXECUTE 'DROP TRIGGER IF EXISTS fk_constraint on ' || child_table_name;
+
+EXECUTE 'CREATE TRIGGER fk_constraint BEFORE INSERT OR UPDATE ON ' || child_table_name || ' FOR EACH ROW
 EXECUTE PROCEDURE
-check_primary_key (REFB, A, ID);
-*/
-/*
-REFB	- name of foreign key column in triggered (B) table. You may use as
-	  many columns as you need, but number of key columns in referenced
-	  table must be the same.
-A	- referenced table name.
-ID	- name of primary key column in referenced table.
-:
-*/
---Trigger for table C:
-/*
-CREATE TRIGGER CT BEFORE INSERT OR UPDATE ON C FOR EACH ROW
-EXECUTE PROCEDURE
-check_primary_key (REFC, A, ID);
-*/
+check_primary_key (
+  ' || child_table_column_names || ',	-- name of foreign key column in triggered (B) table. You may use as
+				-- many columns as you need, but number of key columns in referenced
+				-- table must be the same.
+  ' || parent_table_name || ', -- referenced table name.
+  ' || parent_table_column_names || ')';	-- name of primary key column in referenced table.  
+
 RETURN TRUE;  
 END; 
 $function$
