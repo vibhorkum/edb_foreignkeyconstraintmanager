b90957763263aae7ce8b1c810159ffd68f29b81d
David Thomas <david.thomas@enterprisedb.com>
2016-04-22 12:19:31 -0400

    per #1 if parent is not a partitioned table, use alter table to add a foreign key constraint

--- partition_fk_management.sql
+++ partition_fk_management.sql
@@ -7,22 +7,30 @@ CREATE OR REPLACE FUNCTION public.create_fk_constraint(parent_table_name regclas
 AS $function$
 
 BEGIN
---parent table
-EXECUTE 'DROP TRIGGER IF EXISTS fk_constraint on ' || parent_table_name;
 
-EXECUTE 'CREATE TRIGGER fk_constraint BEFORE DELETE OR UPDATE ON ' || parent_table_name || ' FOR EACH ROW
-EXECUTE PROCEDURE
-check_foreign_key (
-  1,  			-- number of tables that foreign keys need to be checked
-  ' || cascade || ', 	-- boolean defines that corresponding keys must be deleted.
-  ' || array_to_string(parent_table_column_names, ',') || ', 	-- name of primary key column in triggered table (A). 
-							-- You may use as many columns as you need.
-  ' || child_table_name || ', 	-- name of (first) table with foreign keys.
-  ' || array_to_string(child_table_column_names, ',') || ')'; 	-- name of foreign key column in this table. 
+--  If Parent is non-partition and child is partitioned, then use ALTER TABLE add constraint.
+IF EXISTS (select 1 from ALL_PART_TABLES where ALL_PART_TABLES.table_name = upper(parent_table_name::TEXT)) THEN
+
+  -- alter table ALTER TABLE PARENT ADD  FOREIGN KEY(T) REFERENCES CHILD(T);
+  EXECUTE 'ALTER TABLE PARENT ADD  FOREIGN KEY(' || array_to_string(parent_table_column_names, '.') || ') 
+  REFERENCES '|| child_table_name || '('|| array_to_string(child_table_column_names, ',') || ')';
+ELSE
+  --parent table
+  EXECUTE 'DROP TRIGGER IF EXISTS fk_constraint on ' || parent_table_name;
+
+  EXECUTE 'CREATE TRIGGER fk_constraint BEFORE DELETE OR UPDATE ON ' || parent_table_name || ' FOR EACH ROW
+  EXECUTE PROCEDURE
+  check_foreign_key (
+    1,  			-- number of tables that foreign keys need to be checked
+    ' || cascade || ', 	-- boolean defines that corresponding keys must be deleted.
+    ' || array_to_string(parent_table_column_names, ',') || ', 	-- name of primary key column in triggered table (A). 
+ 							-- You may use as many columns as you need.
+    ' || child_table_name || ', 	-- name of (first) table with foreign keys.
+    ' || array_to_string(child_table_column_names, ',') || ')'; 	-- name of foreign key column in this table. 
 							-- You may use as many columns as you need, 
 							-- but number of key columns in referenced table (A) 
 							-- must be the same.
-
+END IF;
 --child table
 
 EXECUTE 'DROP TRIGGER IF EXISTS fk_constraint on ' || child_table_name;
