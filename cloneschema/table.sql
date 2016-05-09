c9cf2923842e0dcb78270f18660fd49d9c06c00a
Phil Vacca <phil.vacca@gmail.com>
2016-05-09 14:26:27 -0500

    Exclude pg internal triggers from copy_trigger (implicitly created with constraints)

--- cloneschema/table.sql
+++ cloneschema/table.sql
@@ -571,7 +571,9 @@ BEGIN
     ) as decl
       , tg.tgname as name
       from pg_catalog.pg_trigger as tg
-     WHERE EXISTS ( SELECT 1 from pg_catalog.pg_class
+     WHERE (NOT tg.tgisinternal 
+        OR (tg.tgisinternal AND tg.tgenabled = 'D'))
+       and EXISTS ( SELECT 1 from pg_catalog.pg_class
        WHERE oid = tg.tgrelid
          and relnamespace = source_schema::regnamespace
        )
