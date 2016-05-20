914373b27b4ec7630140531c32ed3811a797115d
David Thomas <david.thomas@enterprisedb.com>
2016-05-20 10:26:15 -0400

    adding reference to github issue

--- edb_foreignkeyconstraintmanager/tests/test3.sql
+++ edb_foreignkeyconstraintmanager/tests/test3.sql
@@ -1,6 +1,7 @@
 \i inialize_test.sql
 
-\qecho "################ non partitioned parent, non partitioned child #####################"
+\qecho "################ non partitioned parent, non partitioned child #################"
+\qecho "######### https://github.com/EnterpriseDB/kronos_api/issues/15 #################"
 \qecho "################################################################################"
 
 select edb_util.create_fk_constraint('sales_np', '{order_no}', 'sales2_np', '{order_no}', true);
