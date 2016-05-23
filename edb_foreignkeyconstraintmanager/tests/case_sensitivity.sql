90dd9a99a3be1f5229df35684b07f471a09c8a22
David Thomas <david.thomas@enterprisedb.com>
2016-05-23 10:51:07 -0400

    support for mix case table names.  fixes #22

--- edb_foreignkeyconstraintmanager/tests/case_sensitivity.sql
+++ edb_foreignkeyconstraintmanager/tests/case_sensitivity.sql
@@ -1,8 +1,14 @@
 \i inialize_test.sql
 
-\qecho "################ alow multiple keys on a partitioned parent ####################"
+\qecho "################ support case sensitive table names ####### ####################"
 \qecho "#### https://github.com/EnterpriseDB/kronos_api/issues/22 ######################"
 \qecho "################################################################################"
 
-select edb_util.create_fk_constraint('sales', '{order_no}', "SalesNP", '{order_no}', true);
+\qecho "select edb_util.create_fk_constraint('sales', '{order_no}', '"SalesNP"', '{order_no}', true);"
+select edb_util.create_fk_constraint('sales', '{order_no}', '"SalesNP"', '{order_no}', true);
+
+\i inialize_test.sql
+
+\qecho "select edb_util.create_fk_constraint('"SalesNP"', '{order_no}', 'sales', '{order_no}', true);"
+select edb_util.create_fk_constraint('"SalesNP"', '{order_no}', 'sales', '{order_no}', true);
 
